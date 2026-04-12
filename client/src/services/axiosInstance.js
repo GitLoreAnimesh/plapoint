2ea3f028f5778484c6782e857482281b1ef1bfab
animesh <animeshsarker197@gmail.com>
2026-04-13 01:01:46 +0600

    fix: use REACT_APP_API_URL env var for backend URL

--- client/src/services/axiosInstance.js
+++ client/src/services/axiosInstance.js
@@ -10,7 +10,7 @@ import axios from 'axios';
  *   once. If refresh also fails, the user is logged out.
  */
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const axiosInstance = axios.create({
   baseURL:         BASE_URL,
